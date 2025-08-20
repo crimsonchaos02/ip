60d0d54c97cf6f2d674b294d648903468562d33d
Cyrus <schoolprojectcy@gmail.com>
2025-08-21 04:18:40 +0800

    Level 1 - Echo

--- src/main/java/Edith.java
+++ src/main/java/Edith.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+
 public class Edith {
+    public static String pad(String s) {
+        return "==================================\n"
+                + s
+                + "\n==================================";
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String greeting = "=================================="
                 + "\nhello! this is edith :D"
                 + "\nwhat do we need today?"
                 + "\n==================================";
-        String out_message = "jiayousss bye have a great time"
+        String out_message = "=================================="
+                + "\njiayousss bye have a great time"
                 + "\n==================================";
+
         System.out.println(greeting);
-        System.out.println(out_message);
+
+        while (true) {
+            String inp = scanner.nextLine();
+
+            if (inp.equals("bye")) {
+                System.out.println(out_message);
+                break;
+            } else {
+                System.out.println(pad(inp));
+            }
+        }
+
+
     }
 }
