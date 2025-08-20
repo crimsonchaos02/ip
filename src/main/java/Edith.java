c5f49bcf88ca4613b4838d42b6d09cdf061bfae6
Cyrus <schoolprojectcy@gmail.com>
2025-08-21 03:50:42 +0800

    Level 0 -- Renaming to Edith

---
+++ src/main/java/Edith.java
@@ -0,0 +1,12 @@
+public class Edith {
+    public static void main(String[] args) {
+        String greeting = "=================================="
+                + "\nhello! this is edith :D"
+                + "\nwhat do we need today?"
+                + "\n==================================";
+        String out_message = "jiayousss bye have a great time"
+                + "\n==================================";
+        System.out.println(greeting);
+        System.out.println(out_message);
+    }
+}
